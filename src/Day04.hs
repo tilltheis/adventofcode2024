b2529ca957c9d1e6e4e558ee34a04e09296aaf2b
Till Theis <theis.till@gmail.com>
2024-12-23 15:39:40 +0100

    refactor day 4

--- src/Day04.hs
+++ src/Day04.hs
@@ -1,108 +1,43 @@
-module Day04
-  ( Input,
-    parseInput,
-    part1,
-    part2,
-    leftToRight,
-    rightToLeft,
-    topToBottom,
-    bottomToTop,
-    bottomLeftToTopRight,
-    topRightToBottomLeft,
-    topLeftToBottomRight,
-    bottomRightToTopLeft,
-  )
-where
-
-import Data.List (transpose)
+module Day04 (Input, parseInput, part1, part2) where
 
 type Input = [[Char]]
 
 parseInput :: String -> Input
 parseInput = lines
 
-leftToRight :: Input -> Input
-leftToRight = id
-
-rightToLeft :: Input -> Input
-rightToLeft = map reverse
-
-topToBottom :: Input -> Input
-topToBottom = transpose
-
-bottomToTop :: Input -> Input
-bottomToTop = map reverse . topToBottom
-
-bottomLeftToTopRight :: Input -> Input
-bottomLeftToTopRight input =
-  firstHalf ++ secondHalf
-  where
-    firstHalf =
-      [ [ input !! (startRow - col) !! col
-          | col <- [0 .. min startRow (length (head input) - 1)]
-        ]
-        | startRow <- [0 .. length input - 1]
-      ]
-    secondHalf =
-      reverse $
-        [ [ input !! (length input - 1 - row) !! (length (head input) - 1 - (startCol - row))
-            | row <- [0 .. min startCol (length input - 1)]
-          ]
-          | startCol <- [0 .. length (head input) - 2]
-        ]
-
-topRightToBottomLeft :: Input -> Input
-topRightToBottomLeft = reverse . map reverse . bottomLeftToTopRight
-
-topLeftToBottomRight :: Input -> Input
-topLeftToBottomRight input =
-  firstHalf ++ secondHalf
-  where
-    firstHalf =
-      [ [ input !! (length input - 1 - startRow + col) !! col
-          | col <- [0 .. min startRow (length (head input) - 1)]
-        ]
-        | startRow <- [0 .. length input - 1]
-      ]
-    secondHalf =
-      reverse $
-        [ [ input !! row !! (length (head input) - 1 - (startCol - row))
-            | row <- [0 .. min startCol (length input - 1)]
-          ]
-          | startCol <- [0 .. length (head input) - 2]
-        ]
-
-bottomRightToTopLeft :: Input -> Input
-bottomRightToTopLeft = reverse . map reverse . topLeftToBottomRight
-
-slidingWindows :: Int -> [a] -> [[a]]
-slidingWindows n xs
-  | length xs < n = []
-  | otherwise = take n xs : slidingWindows n (tail xs)
-
-part1 :: Input -> Int
-part1 input =
-  sum $
-    map
-      f
-      [ leftToRight input,
-        rightToLeft input,
-        topToBottom input,
-        bottomToTop input,
-        bottomLeftToTopRight input,
-        topRightToBottomLeft input,
-        topLeftToBottomRight input,
-        bottomRightToTopLeft input
-      ]
-  where
-    f = sum . map g
-    g = length . filter (== "XMAS") . slidingWindows 4
-
 count :: (a -> Bool) -> [a] -> Int
 count p = length . filter p
 
+countPatternMatches :: Input -> Input -> Int
+countPatternMatches pattern input = count (== pattern) $ map mask subGrids
+  where
+    patternWidth = length $ head pattern
+    patternHeight = length pattern
+    subGrids = do
+      row <- [0 .. length input - patternHeight]
+      col <- [0 .. length (head input) - patternWidth]
+      return $ map (take patternWidth . drop col) $ take patternHeight $ drop row input
+    mask :: Input -> Input
+    mask subGrid = zipWith (zipWith f) subGrid pattern
+      where
+        f gridChar patternChar = if patternChar == '.' then '.' else gridChar
+
+part1 :: Input -> Int
+part1 input = sum $ map (`countPatternMatches` input) patterns
+  where
+    patterns =
+      [ lines "XMAS",
+        lines "SAMX",
+        lines "X\nM\nA\nS",
+        lines "S\nA\nM\nX",
+        lines "X...\n.M..\n..A.\n...S",
+        lines "S...\n.A..\n..M.\n...X",
+        lines "...X\n..M.\n.A..\nS...",
+        lines "...S\n..A.\n.M..\nX..."
+      ]
+
 part2 :: Input -> Int
-part2 input = count (`elem` patterns) $ map mask subGrids
+part2 input = sum $ map (`countPatternMatches` input) patterns
   where
     patterns =
       [ lines "M.S\n.A.\nM.S",
@@ -110,9 +45,3 @@ part2 input = count (`elem` patterns) $ map mask subGrids
         lines "S.M\n.A.\nS.M",
         lines "S.S\n.A.\nM.M"
       ]
-    subGrids = do
-      row <- [0 .. length input - 3]
-      col <- [0 .. length (head input) - 3]
-      return $ map (take 3 . drop col) $ take 3 $ drop row input
-    mask [[a, _, b], [_, c, _], [d, _, e]] = [[a, '.', b], ['.', c, '.'], [d, '.', e]]
-    mask _ = undefined
--- test/Day04Spec.hs
+++ test/Day04Spec.hs
@@ -19,58 +19,10 @@ input =
 
 spec :: Spec
 spec = do
-  describe "leftToRight" $ do
-    it "transforms the input correctly" $ do
-      leftToRight (parseInput "123\n456") `shouldBe` parseInput "123\n456"
-    it "transforms the input correctly2" $ do
-      leftToRight (parseInput "1234\n5678\n90ab\ncdef") `shouldBe` parseInput "1234\n5678\n90ab\ncdef"
+  describe "part1" $ do
+    it "solves the official example" $ do
+      part1 input `shouldBe` 18
 
-  describe "rightToLeft" $ do
-    it "transforms the input correctly" $ do
-      rightToLeft (parseInput "123\n456") `shouldBe` parseInput "321\n654"
-    it "transforms the input correctly2" $ do
-      rightToLeft (parseInput "1234\n5678\n90ab\ncdef") `shouldBe` parseInput "4321\n8765\nba09\nfedc"
-
-  describe "topToBottom" $ do
-    it "transforms the input correctly" $ do
-      topToBottom (parseInput "123\n456") `shouldBe` parseInput "14\n25\n36"
-    it "transforms the input correctly2" $ do
-      topToBottom (parseInput "1234\n5678\n90ab\ncdef") `shouldBe` parseInput "159c\n260d\n37ae\n48bf"
-
-  describe "bottomToTop" $ do
-    it "transforms the input correctly" $ do
-      bottomToTop (parseInput "123\n456") `shouldBe` parseInput "41\n52\n63"
-    it "transforms the input correctly2" $ do
-      bottomToTop (parseInput "1234\n5678\n90ab\ncdef") `shouldBe` parseInput "c951\nd062\nea73\nfb84"
-
-  describe "bottomLeftToTopRight" $ do
-    it "transforms the input correctly" $ do
-      bottomLeftToTopRight (parseInput "123\n456") `shouldBe` parseInput "1\n42\n53\n6"
-    it "transforms the input correctly2" $ do
-      bottomLeftToTopRight (parseInput "1234\n5678\n90ab\ncdef") `shouldBe` parseInput "1\n52\n963\nc074\nda8\neb\nf"
-
-  describe "topRightToBottomLeft" $ do
-    it "transforms the input correctly" $ do
-      topRightToBottomLeft (parseInput "123\n456") `shouldBe` parseInput "6\n35\n24\n1"
-    it "transforms the input correctly2" $ do
-      topRightToBottomLeft (parseInput "1234\n5678\n90ab\ncdef") `shouldBe` parseInput "f\nbe\n8ad\n470c\n369\n25\n1"
-
-  describe "topLeftToBottomRight" $ do
-    it "transforms the input correctly" $ do
-      topLeftToBottomRight (parseInput "123\n456") `shouldBe` parseInput "4\n15\n26\n3"
-    it "transforms the input correctly2" $ do
-      topLeftToBottomRight (parseInput "1234\n5678\n90ab\ncdef") `shouldBe` parseInput "c\n9d\n50e\n16af\n27b\n38\n4"
-
-    describe "bottomRightToTopLeft" $ do
-      it "transforms the input correctly" $ do
-        bottomRightToTopLeft (parseInput "123\n456") `shouldBe` parseInput "3\n62\n51\n4"
-      it "transforms the input correctly2" $ do
-        bottomRightToTopLeft (parseInput "1234\n5678\n90ab\ncdef") `shouldBe` parseInput "4\n83\nb72\nfa61\ne05\nd9\nc"
-
-    describe "part1" $ do
-      it "solves the official example" $ do
-        part1 input `shouldBe` 18
-
-    describe "part2" $ do
-      it "solves the official example" $ do
-        part2 input `shouldBe` 9
+  describe "part2" $ do
+    it "solves the official example" $ do
+      part2 input `shouldBe` 9
