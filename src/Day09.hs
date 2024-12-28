2ca9a31fb0138d2b115ec4c8269c568675f93877
Till Theis <theis.till@gmail.com>
2024-12-28 17:15:10 +0100

    day 9 2 faster quadratic

--- src/Day09.hs
+++ src/Day09.hs
@@ -2,7 +2,6 @@ module Day09 (Input, parseInput, part1, part2) where
 
 import Data.Char (isSpace)
 import Data.List (dropWhileEnd)
-import Data.Maybe (fromMaybe, isNothing)
 
 type Input = [Int]
 
@@ -24,33 +23,33 @@ part1 input = sum . zipWith (*) [0 ..] $ go 0 expandedLBlocks (length expandedRB
     go i (Nothing : lblocks) j (Nothing : rblocks) = go i (Nothing : lblocks) (j - 1) rblocks
     go _ _ _ _ = undefined
 
-replace :: (Eq a) => a -> a -> [a] -> [a]
-replace _ _ [] = []
-replace from to (x : xs) =
-  if x == from
-    then to : replace from to xs
-    else x : replace from to xs
+replace1 :: (Eq a) => a -> a -> [a] -> [a]
+replace1 _ _ [] = []
+replace1 from to (x : xs) | x == from = to : xs
+replace1 from to (x : xs) = x : replace1 from to xs
 
 part2 :: Input -> Int
-part2 input = sum . zipWith (*) [0 ..] . map (fromMaybe 0) $ moveAll expandedLBlocks expandedRBlocks
+part2 input = sum . zipWith (*) [0 ..] . expand $ moveAll reformattedLBlocks reformattedRBlocks
   where
-    expandedLBlocks = expand 0 input
-    expandedRBlocks = reverse expandedLBlocks
-    expand _ [] = []
-    expand fileId [files] = replicate files (Just fileId)
-    expand fileId (files : frees : blocks) =
-      replicate files (Just fileId) ++ replicate frees Nothing ++ expand (fileId + 1) blocks
+    reformattedLBlocks = reformat 0 input
+    reformattedRBlocks = reverse reformattedLBlocks
+
+    reformat _ [] = []
+    reformat fileId [files] = [(Just fileId, files)]
+    reformat fileId (files : frees : blocks) =
+      (Just fileId, files) : (Nothing, frees) : reformat (fileId + 1) blocks
+
+    expand [] = []
+    expand ((Nothing, size) : blocks) = replicate size 0 ++ expand blocks
+    expand ((Just ident, size) : blocks) = replicate size ident ++ expand blocks
+
     moveAll lblocks [] = lblocks
-    moveAll lblocks (Nothing : rblocks) = moveAll lblocks rblocks
-    moveAll lblocks rblocks@(Just fileId : _) = moveAll lblocks' rblocks'
-      where
-        (files, rblocks') = span (== Just fileId) rblocks
-        lblocks' = moveOne lblocks files
-    moveOne lblocks@(lblock : _) (rblock : _) | lblock == rblock = lblocks
-    moveOne (Just lfile : lblocks) rblocks = Just lfile : moveOne lblocks rblocks
-    moveOne lblocks@(Nothing : _) rblocks@(rblock : _) =
-      let (lfrees, lblocks') = span isNothing lblocks
-       in if length lfrees >= length rblocks
-            then rblocks ++ drop (length rblocks) lfrees ++ replace rblock Nothing lblocks'
-            else lfrees ++ moveOne lblocks' rblocks
-    moveOne _ _ = undefined
+    moveAll lblocks ((Nothing, _) : rblocks) = moveAll lblocks rblocks
+    moveAll lblocks ((Just fileId, size) : rblocks) = moveAll (moveOne lblocks fileId size) rblocks
+
+    moveOne lblocks@((Just lid, _) : _) rid _ | lid == rid = lblocks
+    moveOne ((Nothing, lsize) : lblocks) rid rsize
+      | lsize >= rsize =
+          (Just rid, rsize) : (Nothing, lsize - rsize) : replace1 (Just rid, rsize) (Nothing, rsize) lblocks
+    moveOne (lblock : lblocks) rid rsize = lblock : moveOne lblocks rid rsize
+    moveOne _ _ _ = undefined
